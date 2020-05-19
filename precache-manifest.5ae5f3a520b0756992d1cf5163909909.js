fff090e9f13708f392c65d7daf9db0fe7e727ce3
devanshhalwai <devansh.halwai@gmail.com>
2020-05-20 02:18:43 +0530

    Updates

---
+++ precache-manifest.5ae5f3a520b0756992d1cf5163909909.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab30065a9d55854983111d91e133b877",
+    "url": "/Ristorante-con-Fusion----Devansh-Halwai/index.html"
+  },
+  {
+    "revision": "0ad8524dc7a6b22958d7",
+    "url": "/Ristorante-con-Fusion----Devansh-Halwai/static/css/2.f053de1b.chunk.css"
+  },
+  {
+    "revision": "0e33cd071d9e1c67edf6",
+    "url": "/Ristorante-con-Fusion----Devansh-Halwai/static/css/main.af0cab8a.chunk.css"
+  },
+  {
+    "revision": "0ad8524dc7a6b22958d7",
+    "url": "/Ristorante-con-Fusion----Devansh-Halwai/static/js/2.c48235ec.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Ristorante-con-Fusion----Devansh-Halwai/static/js/2.c48235ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e33cd071d9e1c67edf6",
+    "url": "/Ristorante-con-Fusion----Devansh-Halwai/static/js/main.7f2c8b2d.chunk.js"
+  },
+  {
+    "revision": "efc98822a8bd451b2f3d",
+    "url": "/Ristorante-con-Fusion----Devansh-Halwai/static/js/runtime-main.e5c5f0ab.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Ristorante-con-Fusion----Devansh-Halwai/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Ristorante-con-Fusion----Devansh-Halwai/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Ristorante-con-Fusion----Devansh-Halwai/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Ristorante-con-Fusion----Devansh-Halwai/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Ristorante-con-Fusion----Devansh-Halwai/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
